74ca257ffde486ed68ca85da6a745b588f667537
Álvaro Marques <alvaromarques18@gmail.com>
2023-10-10 17:06:35 -0300

    feat: add section and api implementation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,47 +5,73 @@ import {
   StyleSheet,
   ImageBackground,
   ScrollView,
+  FlatList,
 } from 'react-native';
+import { useState, useEffect } from 'react';
+
 import { HeaderBar } from '../components/HeaderBar';
+import { CardItem } from '../components/CardItem';
+import { Section } from '../components/Sections';
+import { fetchCategories } from '../service/FetchItensByCategory';
+import { Category } from '../types/Product';
 
 export function HomeScreen() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  useEffect(() => {
+    async function getCategories(){
+      const dados = await fetchCategories();
+      setCategories(dados)
+    }
+    getCategories();
+  }, []);
+
+  function listHeader() {
+    return (
+      <ImageBackground
+        source={require('../assets/images/background.png')}
+        style={styles.backgroundImage}
+      >
+        <Image
+          source={require('../assets/images/app-logo.png')}
+          style={styles.logoApp}
+        />
+        <View style={styles.paragraphContainer}>
+          <Text style={styles.paragraph}>Aqui você sempre ganha!</Text>
+          <Image
+            source={require('../assets/images/cart-icon.png')}
+            style={styles.cartIcon}
+          />
+        </View>
+      </ImageBackground>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      
-      <ScrollView>
-          <ImageBackground
-            source={require('../assets/images/background.png')}
-            style={styles.backgroundImage}
-          >
-            <Image
-              source={require('../assets/images/app-logo.png')}
-              style={styles.logoApp}
-            />
-            <View style={styles.paragraphContainer}>
-              <Text>Aqui você sempre ganha!</Text>
-              <Image
-                source={require('../assets/images/cart-icon.png')}
-                style={styles.cartIcon}
-              />
-            </View>
-          </ImageBackground>
-          <View style={styles.categories}>
-
-          </View>
-      </ScrollView>
+      <FlatList
+        ListHeaderComponent={listHeader}
+        data={categories}
+        renderItem = {({item}) => item.name !== "text" ? (
+          <Section 
+          id={item.id}
+          title={item.name}
+          />
+          ) : null} 
+      />
       <HeaderBar isAuthenticated={true} username="Juliane Golçalves Freitas" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "#fff"
+  },
   backgroundImage: {
     height: 374,
-    alignItems:"center",
-    justifyContent: "space-between"
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-
 
   logoApp: {
     marginTop: 160,
@@ -56,12 +82,16 @@ const styles = StyleSheet.create({
   paragraphContainer: {
     alignSelf: 'flex-start',
     flexDirection: 'row',
+    marginLeft: 12,
+    paddingBottom: 16,
     gap: 12,
     alignItems: 'center',
   },
 
   paragraph: {
-    
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
   },
 
   cartIcon: {
@@ -69,7 +99,8 @@ const styles = StyleSheet.create({
     height: 46,
   },
 
-  categories:{
-    backgroundColor: "#fff",
-  }
+  categories: {
+    backgroundColor: '#fff',
+    paddingTop: 25,
+  },
 });
