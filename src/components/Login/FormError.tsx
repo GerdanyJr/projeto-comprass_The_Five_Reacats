635994289a8abc43eef6cc20c991b4481f67bef0
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-10 20:09:43 -0300

    feat: create Form Error component

---
+++ src/components/Login/FormError.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { Colors } from '../../assets/constants/Colors';
+
+export function FormError({ message }: { message: string }) {
+  return <Text style={styles.error}>{message}</Text>;
+}
+
+const styles = StyleSheet.create({
+  error: {
+    color: Colors.red_200,
+    fontWeight: '400',
+    fontSize: 16,
+  },
+});
