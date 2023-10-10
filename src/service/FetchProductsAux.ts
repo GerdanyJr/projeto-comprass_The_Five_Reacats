f163436c53f6309d8c23c43126f037f85722a8e6
Álvaro Marques <alvaromarques18@gmail.com>
2023-10-10 20:51:09 -0300

    style: rename fetchItensBy to FetchProductsAux

--- src/service/FetchProductsAux.ts
+++ src/service/FetchProductsAux.ts
@@ -3,7 +3,7 @@ import {Product, Category} from '../types/Product';
 
 export async function fetchItensByCategory(id:string){
   const response = await axios.get(`https://api.escuelajs.co/api/v1/categories/${id}/products`);
-  const data = response.data;
+  const data:Product[] = response.data;
   return data;
 }
 
