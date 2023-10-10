5947265752d7e0e81ccca0594f72a5bda2216236
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-10 13:33:44 -0300

    feat: create Colors constant

---
+++ src/assets/constants/Colors.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  black: '#212121',
+  black_800: '#111213',
+  white: '#FFFFFF',
+  red_500: '#FF0024',
+  red_200: '#EA6275',
+  gray_900: '#565656',
+  gray_500: '#9B9B9B',
+  gray_100: '#B6B6B6',
+  yellow_900: '#FFCE00',
+  green_900: '#2AA952',
+  orange_900: '#FF8000',
+};
