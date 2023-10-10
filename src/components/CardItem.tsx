4686d51ba18b0e4658cde244089c3708d94c1e31
Álvaro Marques <alvaromarques18@gmail.com>
2023-10-10 02:10:50 -0300

    feat: add card item

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,111 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+} from 'react-native';
+import { useState } from 'react';
+
+export function CardItem({name, description, price}: {name:string, description:string, price:string}) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.countContainer}>
+        <Pressable onPress={() => {(count !== 0) ? setCount(count -1 ) : setCount(count)}} style={[styles.button, styles.leftCorner]}>
+          <Image source={require('../assets/images/minus-icon.png')} style={styles.minus}/>
+        </Pressable>
+        <Text style={styles.count}>{count}</Text>
+        <Pressable onPress={() => {setCount(count + 1)}} style={[styles.button, styles.rightCorner]}>
+          <Image source={require('../assets/images/plus-icon.png')} style={styles.plus}/>
+        </Pressable>
+      </View>
+      <Pressable style={styles.productContainer}>
+        <Image source={require("../assets/images/product-example.png")} style={styles.productImage}/>
+        <View>
+          <Text style={styles.productName}>{name}</Text>
+          <Text style={styles.productDescription}>{description}</Text>
+          <Text style={styles.productPrice}>{price} R$</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 148,
+  },
+
+  countContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: 'row',
+  },
+
+  button:{
+    backgroundColor: "#FF0024",
+    width: 50,
+    height: 21,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  leftCorner: {
+    borderTopLeftRadius: 12,
+  },
+
+  rightCorner:{
+    borderTopRightRadius: 12
+  },
+
+  minus: {
+    width: 14,
+    height: 3,
+    borderRadius: 13,
+  },
+
+  count:{
+    width: 46,
+    textAlign: "center",
+    color: "#000",
+    backgroundColor: "#fff",
+    borderColor: "#9B9B9B",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+
+  plus: {
+    width: 14,
+    height: 13,
+  },
+
+  productContainer:{
+
+  },
+
+  productImage: {
+    width: 148,
+    height: 184,
+  },
+
+  productName:{
+    marginTop: 8,
+    color: "#9B9B9B",
+    fontSize: 14,
+    fontWeight: "700"
+  },
+  productDescription:{
+    marginTop: 4,
+    color: "#000",
+    fontSize: 10,
+    fontWeight: "400"
+  },
+  productPrice:{
+    marginTop: 6,
+    color: "#FF0024",
+    fontSize: 16,
+    fontWeight: "800"
+  },
+});
