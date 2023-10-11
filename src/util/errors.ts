7ee0e785806dfa1831b1a834bf99daa47baa1ff0
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-11 03:37:51 -0300

    feat: create get Sign Up Form Error method

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,5 +1,6 @@
 import { FieldErrors } from 'react-hook-form';
 import { Inputs } from '../screens/LoginScreen';
+import { SignUpInputs } from '../screens/SignUpScreen';
 
 export function getErrorMessageByCode(httpCode: number): string {
   switch (httpCode) {
@@ -12,10 +13,21 @@ export function getErrorMessageByCode(httpCode: number): string {
   }
 }
 
-export function getFormErrorMessage(errors: FieldErrors<Inputs>) {
+export function getLoginFormErrorMessage(errors: FieldErrors<Inputs>) {
   if (errors.email?.message) {
     return errors.email.message;
   } else if (errors.password?.message) {
     return errors.password.message;
+  } else return '';
+}
+export function getSignUpFormErrorMessage(errors: FieldErrors<SignUpInputs>) {
+  if (errors.email?.message) {
+    return errors.email.message;
+  } else if (errors.password?.message) {
+    return errors.password.message;
+  } else if (errors.confirmPassword?.message) {
+    return errors.confirmPassword.message;
+  } else if (errors.name?.message) {
+    return errors.name.message;
   } else return '';
 }
