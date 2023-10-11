70d2c7930533c3ab4528f51f7f9958b3af84b031
Álvaro Marques <alvaromarques18@gmail.com>
2023-10-11 02:36:18 -0300

    feat: add  cardExample

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet, Pressable, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
 import { CardItem } from './CardItem';
-import { fetchItensByCategory } from '../service/FetchItensByCategory';
+import { CardItemExemple } from './CardItemExemple';
+import { fetchItensByCategory } from '../service/FetchProductsAux';
 import { Product } from '../types/Product';
 
-export function Section({ id, title }: { id: string, title:string }) {
+export function Section({ id, title }: { id: string; title: string }) {
   const [produtos, setProdutos] = useState<Product[]>([]);
   useEffect(() => {
     async function getItensByCategory() {
@@ -16,19 +17,27 @@ export function Section({ id, title }: { id: string, title:string }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.categoryTitle}>{title}</Text>
+      <View style={styles.categoryHeader}>
+        <Text style={styles.categoryTitle}>{title}</Text>
+        <Pressable style={styles.buttonViewAll}>
+          <Text style={styles.buttonViewAllText}>View all</Text>
+        </Pressable>
+      </View>
       <FlatList
         data={produtos}
-        renderItem={({ item }) => (
-          <CardItem
-            name={item.title}
-            description={item.description}
-            price={item.price}
-            url={item.images[0]}
-          />
-        )}
+        renderItem={({ item }) =>
+          produtos.length !== 0 ? (
+            <CardItem
+              name={item.title}
+              description={item.description}
+              price={item.price}
+              url={item.images[0]}
+            />
+          ) : (
+            <CardItemExemple />
+          )
+        }
         horizontal={true}
-        maxToRenderPerBatch={3}
       />
     </View>
   );
@@ -40,11 +49,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 
+  categoryHeader: {
+    width: 343,
+    height: 44,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+
   categoryTitle: {
     color: '#000',
     marginLeft: 16,
     fontSize: 32,
     fontWeight: '800',
-    marginBottom: 16,
+  },
+
+  buttonViewAll: {
+    justifyContent: 'center',
+  },
+
+  buttonViewAllText: {
+    color: '#000',
+    fontWeight: '400',
+    fontSize: 12,
   },
 });
