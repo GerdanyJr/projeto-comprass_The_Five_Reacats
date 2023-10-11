2e166010ac1cbf1c3811876e6d4b69e01e00d539
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-10 22:19:56 -0300

    feat: improve error handling

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,10 +1,21 @@
+import { FieldErrors } from 'react-hook-form';
+import { Inputs } from '../screens/LoginScreen';
+
 export function getErrorMessageByCode(httpCode: number): string {
-    switch(httpCode) {
-      case 401:
-        return "Your email or password is incorrect";
-      case 403:
-        return "Forbidden. You do not have permission to access this resource.";
-      default:
-        return "Something happened, try again later";
-    }
+  switch (httpCode) {
+    case 401:
+      return 'Your email or password is incorrect';
+    case 403:
+      return 'Forbidden. You do not have permission to access this resource.';
+    default:
+      return 'Something happened, try again later';
   }
+}
+
+export function getFormErrorMessage(errors: FieldErrors<Inputs>) {
+  if (errors.email?.message) {
+    return errors.email.message;
+  } else if (errors.password?.message) {
+    return errors.password.message;
+  } else return '';
+}
