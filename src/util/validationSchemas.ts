fd00659cb8c3f9f9b5bc2b6797a0b61adc66e023
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-12 00:12:46 -0300

    feat: create forgot password schema

--- src/util/validationSchemas.ts
+++ src/util/validationSchemas.ts
@@ -15,3 +15,18 @@ export const signUpSchema = yup.object({
         'Your password is not the same as your confirmation'
       ),
   });
+
+export const forgotPasswordSchema = yup.object({
+    email: yup
+      .string()
+      .required('Please enter your email')
+      .email('Please insert a valid email'),
+    password: yup.string().required('Please enter your password').min(6),
+    confirmPassword: yup
+      .string()
+      .required('Please confirm your password')
+      .oneOf(
+        [yup.ref('password')],
+        'Your password is not the same as your confirmation'
+      ),
+  });
