bed95e0a499a888b8340a31701bf4e460f0646fa
Luiz gustavo V. Matos <contatoluizgustavoig@gmail.com>
2023-10-13 12:37:11 -0300

    feat: add InvoicePayment screen

---
+++ src/screens/InvoicePaymentScreen.tsx
@@ -0,0 +1,76 @@
+import React, {useEffect} from "react";
+import { View, StyleSheet, Text} from 'react-native';
+import ContinueShoppingButton from "../components/Success/ContinueShoppingButton";
+import BagsBackgroundImage from "../components/Success/BagsBackgroundImage";
+import SuccessText from "../components/Success/SuccessText";
+import DownloadInvoiceButton from "../components/Success/DownloadInvoiceButton";
+import InvoicePaymentText from "../components/Success/InvoicePaymentText";
+import { Colors } from "../assets/constants/Colors"; 
+import { useNavigation } from "@react-navigation/native";
+
+const InvoicePaymentScreen: React.FC = () => {
+
+    const navigation = useNavigation();
+  
+    useEffect(() => {
+      navigation.setOptions({
+        headerShown: false,
+      });
+  
+    })
+    
+    const handleContinuePress = () => {
+      
+    };
+    const downloadInvoicePress = () => {
+
+    };
+  
+    return (
+      <View style={styles.container}>
+        <BagsBackgroundImage source={require('../assets/images/bags.png')}>
+        </BagsBackgroundImage>
+        <View style={styles.SuccessText}>
+            <SuccessText  />
+            <InvoicePaymentText
+            style={styles.instructionText}
+            daysOffset={1}
+            />
+        </View>
+        <DownloadInvoiceButton 
+        onPress={downloadInvoicePress}
+        children= 'BAIXAR BOLETO'
+        />
+          <ContinueShoppingButton
+          onPress={handleContinuePress}
+          children= 'CONTINUE SHOPPING'
+          />
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: Colors.white,
+
+    },
+    instructionText: {
+      color: Colors.black_800,
+      lineHeight: 24,
+      fontSize: 16,
+      fontWeight: '400',
+      textAlign: 'center',
+      paddingBottom: 16,
+  
+    },
+    SuccessText: {
+        marginTop: 16,
+        marginBottom: 310,
+    }
+  });
+
+  export default InvoicePaymentScreen;
+  
