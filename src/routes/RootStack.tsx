7efe4a406132b8be7e341632cea8758e8bce2a95
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-13 12:45:13 -0300

    feat: create User Context

--- src/routes/RootStack.tsx
+++ src/routes/RootStack.tsx
@@ -3,15 +3,18 @@ import { MainPage } from './MainPage/MainPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthStack } from './Auth/AuthStack';
+import { UserContextProvider } from '../store/UserContext';
 
 const Stack = createNativeStackNavigator();
 export function RootStack(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="AuthStack" component={AuthStack} />
-        <Stack.Screen name="MainPage" component={MainPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="MainPage" component={MainPage} />
+          <Stack.Screen name="AuthStack" component={AuthStack} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContextProvider>
   );
 }
