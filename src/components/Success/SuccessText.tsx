859b8ccb0680f3754ccd7ae0a947f5317a7fab8d
Luiz gustavo V. Matos <contatoluizgustavoig@gmail.com>
2023-10-13 00:28:37 -0300

    feat: add SuccessText component

---
+++ src/components/Success/SuccessText.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { Colors } from '../../assets/constants/Colors';
+
+const SuccessText: React.FC = () => {
+  return <Text style={styles.successText}>Success!</Text>;
+};
+
+const styles = StyleSheet.create({
+  successText: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: Colors.black_800,
+    textAlign: 'center',
+    paddingBottom: 12,
+    
+  },
+});
+
+export default SuccessText;
