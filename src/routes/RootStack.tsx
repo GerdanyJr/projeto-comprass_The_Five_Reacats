d823fa68caa02eca001c08b63d81ab8438a73ab8
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-14 01:04:10 -0300

    feat: add Checkout stack to root

--- src/routes/RootStack.tsx
+++ src/routes/RootStack.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthStack } from './Auth/AuthStack';
 import { UserContextProvider } from '../store/UserContext';
+import { CheckoutStack } from './Checkout/CheckoutStack';
 
 const Stack = createNativeStackNavigator();
 export function RootStack(): JSX.Element {
@@ -13,6 +14,7 @@ export function RootStack(): JSX.Element {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="MainPage" component={MainPage} />
           <Stack.Screen name="AuthStack" component={AuthStack} />
+          <Stack.Screen name='CheckoutStack' component={CheckoutStack} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserContextProvider>
