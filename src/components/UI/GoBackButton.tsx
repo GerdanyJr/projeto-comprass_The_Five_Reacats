1f9de08e83bbf5f281835b89e11d4da4ca411584
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-14 01:02:29 -0300

    feat: add black color to go back

--- src/components/UI/GoBackButton.tsx
+++ src/components/UI/GoBackButton.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Image, Pressable } from 'react-native';
 
 interface GoBackButtonProps {
+  black?: boolean;
   onPress: () => void;
 }
 
-export function GoBackButton({ onPress }: GoBackButtonProps) {
+export function GoBackButton({ black, onPress }: GoBackButtonProps) {
   return (
     <Pressable onPress={onPress}>
-      <Image source={require('../../assets/images/go-back.png')} />
+      <Image
+        source={
+          black
+            ? require('../../assets/images/go-back-black.png')
+            : require('../../assets/images/go-back.png')
+        }
+      />
     </Pressable>
   );
 }
