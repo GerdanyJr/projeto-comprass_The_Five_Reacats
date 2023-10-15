9971e7eceefadcbea039dcb383ce0608b9e20dfe
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-15 02:20:53 -0300

    docs: change logos constant folder

---
+++ src/util/constants/logos.ts
@@ -0,0 +1,29 @@
+import { DeliveryMethod } from '../../types/interfaces/DeliveryMethod';
+
+export const logos: DeliveryMethod[] = [
+  {
+    id: 0,
+    name: 'fedex',
+    logo: require('../../assets/images/fedex-logo.png'),
+  },
+  {
+    id: 1,
+    name: 'dhl',
+    logo: require('../../assets/images/dhl-logo.png'),
+  },
+  {
+    id: 2,
+    name: 'fedex',
+    logo: require('../../assets/images/usps-logo.png'),
+  },
+  {
+    id: 3,
+    name: 'fedex',
+    logo: require('../../assets/images/fedex-logo.png'),
+  },
+  {
+    id: 4,
+    name: 'fedex',
+    logo: require('../../assets/images/fedex-logo.png'),
+  },
+];
