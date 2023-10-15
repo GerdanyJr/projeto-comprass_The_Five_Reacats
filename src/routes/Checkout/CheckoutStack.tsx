ecde48e174d12821249bc8deba5746da779152a2
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-14 21:49:03 -0300

    refactor: split screen options from Checkout Stack

--- src/routes/Checkout/CheckoutStack.tsx
+++ src/routes/Checkout/CheckoutStack.tsx
@@ -1,35 +1,28 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CheckoutScreen } from '../../screens/CheckoutScreen';
-import { useNavigation } from '@react-navigation/native';
 import { ShippingAddressForm } from '../../components/Checkout/ShippingAddressForm';
-import { GoBackButton } from '../../components/UI/GoBackButton';
+import { CardForm } from '../../components/Checkout/CardForm';
+import { checkoutStackOptions } from './CheckoutStackOptions';
+import { CheckoutContextProvider } from '../../store/CheckoutContext';
 
 const Stack = createNativeStackNavigator();
 export function CheckoutStack() {
-  const navigation = useNavigation<any>();
   return (
-    <Stack.Navigator
-      screenOptions={({ navigation }) => ({
-        headerLeft: () => (
-          <GoBackButton
-            black
-            onPress={() => {
-              navigation.goBack();
-            }}
-          />
-        ),
-        headerShadowVisible: false,
-        headerTitleAlign: 'center',
-        headerTitleStyle: { fontSize: 22, fontWeight: '700' },
-      })}
-    >
-      <Stack.Screen name="Checkout" component={CheckoutScreen} />
-      <Stack.Screen
-        name="ShippingAddressForm"
-        component={ShippingAddressForm}
-        options={{ presentation: 'modal', animation: 'slide_from_bottom' }}
-      />
-    </Stack.Navigator>
+    <CheckoutContextProvider>
+      <Stack.Navigator screenOptions={checkoutStackOptions.navigatorOptions}>
+        <Stack.Screen name="Checkout" component={CheckoutScreen} />
+        <Stack.Screen
+          name="ShippingAddressForm"
+          component={ShippingAddressForm}
+          options={checkoutStackOptions.shippingAddress}
+        />
+        <Stack.Screen
+          name="CardForm"
+          component={CardForm}
+          options={checkoutStackOptions.cardForm}
+        />
+      </Stack.Navigator>
+    </CheckoutContextProvider>
   );
 }
