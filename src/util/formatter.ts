2876010ba1391a270d38af79eb4c2548fe6b51e5
Gerdany Junior <Asa_lindo@hotmail.com>
2023-10-15 16:26:15 -0300

    feat: create card formatter methods

--- src/util/formatter.ts
+++ src/util/formatter.ts
@@ -4,10 +4,39 @@ export function formatCep(cep: string) {
   } else return cep;
 }
 
-function formatCurrency(amount: number) {
+export function formatCurrency(amount: number) {
   const formattedAmount = amount.toLocaleString(undefined, {
     style: 'currency',
     currency: 'USD',
   });
   return formattedAmount;
+}
+
+export function formatCardNumber(number: string) {
+  const cardNumber = number.replace(/\D/g, '');
+  const groups = [4, 4, 4, 4];
+  let formattedCardNumber = '';
+  let currentIndex = 0;
+
+  for (const groupSize of groups) {
+    const group = cardNumber.slice(currentIndex, currentIndex + groupSize);
+    if (group.length > 0) {
+      formattedCardNumber += group + ' ';
+    }
+    currentIndex += groupSize;
+  }
+
+  if (formattedCardNumber.endsWith(' ')) {
+    formattedCardNumber = formattedCardNumber.slice(0, -1);
+  }
+  return formattedCardNumber;
+}
+
+export function formatExpireDate(date: string) {
+  const expireDate = date.replace(/\D/g, '');
+  if (expireDate.length > 2) {
+    return expireDate.slice(0, 2) + '/' + expireDate.slice(2);
+  } else {
+    return expireDate;
+  }
 }
