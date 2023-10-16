a569dcb0a56ce1bfe630cfc78ce429803b72a520
Álvaro Marques <alvaromarques18@gmail.com>
2023-10-16 01:19:27 -0300

    feat: create product screen

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,17 +1,49 @@
-import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  ScrollView,
+} from 'react-native';
 import { Carousel } from '../components/Product/Carousel';
+import { CounterProduct } from '../components/Product/CounterProduct';
+import { MoreInfoButtons } from '../components/Product/MoreInfoButtons';
+import { Section } from '../components/Product/Section';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-export function ProductScreen({ navigation }: { navigation?: any }) {
+
+export function ProductScreen({
+  route
+}: {
+  route: any;
+}) {
+  var { itemId, categoryId } = route.params;
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
   return (
     <View style={styles.container}>
-      <View style={styles.headerBar}>
-        <Pressable style={styles.goBackButton}>
-          <Image source={require('../assets/images/go-back-dark.png')} />
-        </Pressable>
-        <Text style={styles.headerText}>Details</Text>
-      </View>
+      <ScrollView style={styles.list}>
+        <View style={styles.headerBar}>
+          <Pressable style={styles.goBackButton} onPress={()=>{navigation.navigate("Home")}
+        }>
+            <Image source={require('../assets/images/go-back-dark.png')} />
+          </Pressable>
+          <Text style={styles.headerText}>Details</Text>
+        </View>
 
-      <Carousel id={"12"}/>
+        <Carousel id={JSON.stringify(itemId)} />
+        <MoreInfoButtons />
+
+        <Section
+          title="You can also like this"
+          id={JSON.stringify(categoryId)}
+        />
+      </ScrollView>
+      <View style={styles.bottomBar}>
+        <CounterProduct />
+      </View>
     </View>
   );
 }
@@ -20,18 +52,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 
+  list: {
+    height: '85%',
+  },
+
+  bottomBar: {
+    alignSelf: 'center',
+    verticalAlign: 'bottom',
+    backgroundColor: '#fff',
+    height: '15%',
+  },
+
   headerBar: {
     marginTop: 10,
-    marginLeft: 16,
+    marginHorizontal: 16,
     flexDirection: 'row',
-    gap: 127,
   },
 
   goBackButton: {
+    width: 25,
     marginTop: 6,
   },
 
   headerText: {
+    textAlign: 'center',
+    width: '90%',
     fontWeight: '700',
     fontSize: 18,
     color: '#222222',
