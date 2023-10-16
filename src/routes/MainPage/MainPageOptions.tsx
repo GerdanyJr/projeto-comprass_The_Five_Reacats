c1b9de30ba1a973e85b79b87ed43d71cdace7849
Álvaro Marques <alvaromarques18@gmail.com>
2023-10-16 01:20:50 -0300

    feat: add badge to tabBarIcon

--- src/routes/MainPage/MainPageOptions.tsx
+++ src/routes/MainPage/MainPageOptions.tsx
@@ -6,9 +6,8 @@ export const mainPageOptions = {
     headerShown: false,
     tabBarActiveTintColor: Colors.red_500,
     tabBarLabelStyle: { fontSize: 14 },
-    
   },
-  homeOptions: {
+  mainOptions: {
     tabBarIcon: ({ color, size, focused }: any) => (
       <Ionicons
         name={focused ? 'home' : 'home-outline'}
@@ -25,9 +24,14 @@ export const mainPageOptions = {
         size={size}
       />
     ),
+    tabBarBadge: "activated",
+    tabBarBadgeStyle: {
+      maxWidth: 6,
+      maxHeight: 8,
+    },
   },
   profileOptions: {
-    tabBarIcon: ({ color, size, focused }:any) => (
+    tabBarIcon: ({ color, size, focused }: any) => (
       <Ionicons
         name={focused ? 'person' : 'person-outline'}
         color={color}
